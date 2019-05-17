a6faadab9e7ca16756046cd8f9da545c7e490b3d
0/0 <dev0p0@users.noreply.github.com>
2019-05-17 05:49:55 -0900

    Changing comments per request by a friend!

--- BCracker.py
+++ BCracker.py
@@ -1,20 +1,20 @@
 import os
 import bitcoin
 
-"""This file 'Bits.txt' would contain the addresses you would want to bruteforce"""
+#This file 'Bits.txt' would contain the addresses you would want to bruteforce
 
 filexy = open('Bits.txt',mode='r')
 
-"""Loading all the addresses to memory for fast search
+#Loading all the addresses to memory for fast search
 bit_addresses = filexy.read()
 
 filexy.close()
 
-"""xchoicesx is structured this way so that lower part of the stack of possible private keys also have a chance to be searched cause if only a simple os.urandom for 2**255 possible private keys, most of the time only really big private keys are generated and tinier private keys don't usually have much chance to be searched!
+#xchoicesx is structured this way so that lower part of the stack of possible private keys also have a chance to be searched cause if only a simple os.urandom for 2**255 possible private keys, most of the time only really big private keys are generated and tinier private keys don't usually have much chance to be searched!
 xchoicesx = [2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16]
 
 
-"""This fucntion Generates a private key
+#This fucntion Generates a private key
 
 def Generate_Private_key_hex():
     while True:
@@ -29,17 +29,17 @@ def Generate_Private_key_hex():
 inmbr = 0
 found_nmbr = 0
 
-"""The file 'bitsybitsy.txt' would contain all the found bitcoin addresses that have been bruteforced and have been found in the 'Bits.txt' file
+#The file 'bitsybitsy.txt' would contain all the found bitcoin addresses that have been bruteforced and have been found in the 'Bits.txt' file
 filex2 = open('bitsybitsy.txt', 'a')
 
 while True:
     inmbr += 1
     if inmbr % 50000 == 0:
-        """Every 50000 iterations that's been done print a notice so we know how far we have come
+        #Every 50000 iterations that's been done print a notice so we know how far we have come
         print(inmbr)
     private_key_hex = Generate_Private_key_hex()
     if bitcoin.pubkey_to_address(bitcoin.compress(bitcoin.privkey_to_pubkey(private_key_hex))) in bit_addresses:
-        """Every bitcoin private key has two types of addresses either a compressed address or an uncompressed one, here we search the compressed version
+        #Every bitcoin private key has two types of addresses either a compressed address or an uncompressed one, here we search the compressed version
         found_nmbr += 1
         print(private_key_hex)
         filex2.write(private_key_hex + "\n")
@@ -56,7 +56,7 @@ while True:
         filex2.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + "\n")
         filex2.flush()
     if  bitcoin.pubkey_to_address(bitcoin.privkey_to_pubkey(private_key_hex)) in bit_addresses:
-        """"Every bitcoin private key has two types of addresses either a compressed address or an uncompressed one, here we search the uncompressed version
+        #Every bitcoin private key has two types of addresses either a compressed address or an uncompressed one, here we search the uncompressed version
         found_nmbr += 1
         print(private_key_hex)
         filex2.write(private_key_hex + "\n")
